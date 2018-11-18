6ac9f98af7d4ecf5f74aaf60d11a59cefd2e9ed7
Alejafranco <mafa1008@gmail.com>
2018-11-18 15:51:57 -0500

    Tarea testing

--- codebreaker.js
+++ codebreaker.js
@@ -1,9 +1,33 @@
-var realNumber = "1234";
+var realNumber = "5612";
 
- const guess = function(number) {
+ const guess = function(number1) {
   // TODO: Logic Here
+  let result = "";
 
-  return "XXXX";
+  let real = (realNumber.split(""));
+  let actual = number1.split("");
+
+  for (var i=0; i<realNumber.length; i++){
+    for (var y=0; y<number1.length; y++){
+      if(realNumber[i] === number1[y]){
+        if(i == y){
+          result = result + "x";
+        }else{
+        result = result + "o";
+        }
+      }
+    }
+  }
+
+ /*actual.forEach(element => {
+  //Si element esta en real so add una o ó una x a result
+ });*/
+
+  return result;
 }
+
+/*const setNumber = function (number1){
+  realNumber = number1;
+}*/
 
 module.exports = guess;
--- test/codebreaker.test.js
+++ test/codebreaker.test.js
@@ -8,8 +8,66 @@ describe( 'Test pass', function(){
   })
 })
 
+//5612
+
 describe('Codebreaker Test', function(){
-    it('This is a test case', function(){
-        // TODO: Test case here
+    it ('Debe retornar un o', function(){
+        let result = guess("1987")
+
+        test.equal(result, "o", "Tiene un número correcto en la posición incorrecta");
+    })
+
+    it ('Debe retornar un xo', function(){
+        let result = guess("5469")
+
+        test.equal(result, "xo", "Tiene un numero en la posicion correcta y otro no ");
+    })
+
+    it ('Debe retornar un oo', function(){
+        let result = guess("2345")
+
+        test.equal(result, "oo", "Tiene dos numeros correctos en la posicion equivocada");
+    })
+
+    it ('Debe retornar un ox', function(){
+        let result = guess("0592")
+
+        test.equal(result, "ox", "Tiene dos numeros correctos uno en la posicion equivocada y otro no");
+    })
+
+    it ('Debe retornar un x', function(){
+        let result = guess("5794")
+
+        test.equal(result, "x", "Tienes un número correcto!");
+    })
+
+    it ('Debe retornar un xx', function(){
+        let result = guess("5714")
+
+        test.equal(result, "xx", "Tienes dos números correcto!");
+    })
+
+    it ('Debe retornar un xox', function(){
+        let result = guess("3652")
+
+        test.equal(result, "xox", "Tienes dos número en la posicion correcta y uno no!");
+    })
+
+    it ('Debe retornar un oxo', function(){
+        let result = guess("2816")
+
+        test.equal(result, "oxo", "Tienes dos número en la posicion correcta y uno no!");
+    })
+
+    it ('Debe retornar un oxox', function(){
+        let result = guess("1652")
+
+        test.equal(result, "oxox", "Tienes dos número en la posicion correcta y uno no!");
+    })
+
+    it ('Debe retornar un xoox', function(){
+        let result = guess("5162")
+
+        test.equal(result, "xoox", "Tienes dos número en la posicion correcta y uno no!");
     })
 })
